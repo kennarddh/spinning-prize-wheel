37eab082a54c0caed6fa42b68d4420817b8527ff
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-12-02 18:39:26 +0700

    docs: Wheel props

--- src/Components/LuckyWheel/Types.ts
+++ src/Components/LuckyWheel/Types.ts
@@ -12,15 +12,49 @@ export type IChoice = {
 	label: string
 	color: string
 	rarityGroup: string
-}[]
+}
+
+export type IChoices = IChoice[]
 
 export interface IProps {
+	/**
+	 * Rotate duration
+	 */
 	rotateDuration: number
+
+	/**
+	 * One of rarity key in rarity groups
+	 */
 	rarityGroups: IRarityGroups
+
+	/**
+	 * Avaliavle choices
+	 */
 	choice: IChoice
+
+	/**
+	 * Remove default arrow
+	 */
 	withoutArrow?: boolean
-	fullRotationAddBeforeDestination?: FactoryOrValue<number> // Default 2 - 5 random
+
+	/**
+	 * Full rotation count before rotate to destination can be given as factory or value
+	 *
+	 * Default 2 - 5 rotation randomized
+	 */
+
+	fullRotationAddBeforeDestination?: FactoryOrValue<number>
+
+	/**
+	 * Change rotate direction to counter clockwise
+	 */
 	counterClockwise?: boolean
+
+	/**
+	 * Callback that called when rotation completed
+	 *
+	 * @param {string} id - Choosed id
+	 */
 	onEndRotate: ((id: string) => void) | undefined
 }
 
